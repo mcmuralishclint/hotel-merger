70becf1095ce9e5ef9597920cab9b58edccc12a5
Muralish <mcmuralishclint@outlook.com>
2023-12-29 02:47:28 +0530

    update the search controller to query and return hotel info

--- app/controllers/api/v1/hotels_controller.rb
+++ app/controllers/api/v1/hotels_controller.rb
@@ -1,12 +1,29 @@
-
 module Api
-    module V1
-      class HotelsController < ApplicationController
-        def search
-          hotel = Hotel.first
-          render json: hotel, serializer: HotelSerializer
+  module V1
+    ALLOWED_SEARCH_TYPE = ["destination_id", "id"]
+
+    class HotelsController < ApplicationController
+      before_action :validate_search_params
+
+      def search
+        search_type = params[:search_type]
+        search_value = params[:search_value]
+
+        hotels = Hotel.where(search_type => search_value)
+        render json: hotels, each_serializer: HotelSerializer
+      end
+
+      private
+
+      def validate_search_params
+        unless params[:search_type].present? && params[:search_value].present?
+          return render json: { error: 'Search parameters are missing' }, status: :unprocessable_entity
+        end
+
+        unless ALLOWED_SEARCH_TYPE.include?(params[:search_type])
+          return render json: { error: 'Invalid search type' }, status: :unprocessable_entity
         end
       end
     end
   end
-  
+end
