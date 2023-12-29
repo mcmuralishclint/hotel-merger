8497f207a5330dbb72f20b32db9a4115eb897e15
Muralish <mcmuralishclint@outlook.com>
2023-12-29 19:25:34 +0530

    patagonia service and builder

--- app/services/supplier/patagonia_service.rb
+++ app/services/supplier/patagonia_service.rb
@@ -1,5 +1,13 @@
 module Supplier
   class PatagoniaService < BaseService
+    def parse_hotel_params(hotel_params)
+      Supplier::Builder::PatagoniaBuilder.new(hotel_params).build_params
+    end
 
+    private
+
+    def path
+      "https://5f2be0b4ffc88500167b85a0.mockapi.io/suppliers/patagonia"
+    end
   end
 end
