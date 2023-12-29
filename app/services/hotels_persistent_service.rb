93ad3dbf0b3c88dbefb7941e219046b5e5f73068
Muralish <mcmuralishclint@outlook.com>
2023-12-30 01:45:05 +0530

    HotelsPersistentService: methods to prevent duplication and gain granular control

--- app/services/hotels_persistent_service.rb
+++ app/services/hotels_persistent_service.rb
@@ -1,3 +1,6 @@
+# The purpose of this class is to provide an interface to get granular
+# control over each attribute. The class is open for future improvements to
+# control the values to retain post merge
 class HotelsPersistentService
   def initialize(hotel_params)
     @hotel_params = hotel_params.with_indifferent_access
@@ -12,18 +15,18 @@ class HotelsPersistentService
 
   def validate_params
     validated_params = {}
-    validated_params['name'] = validate_name(@hotel_params['name']&.strip)
-    validated_params['description'] = validate_description(@hotel_params['description'])
-    validated_params['destination_id'] = validate_destination_id(@hotel_params['destination_id'])
-    validated_params['location_attributes'] = validate_location_attributes(@hotel_params['location_attributes'])
-    validated_params['amenities_attributes'] = validate_amenities_attributes(@hotel_params['amenities_attributes'])
-    validated_params['images_attributes'] = validate_images_attributes(@hotel_params['images_attributes'])
-    validated_params['booking_conditions_attributes'] = validate_booking_conditions_attributes(@hotel_params['booking_conditions_attributes'])
+    validated_params['name'] = validate_and_modify_name(@hotel_params['name']&.strip)
+    validated_params['description'] = validate_and_modify_description(@hotel_params['description'])
+    validated_params['destination_id'] = validate_and_modify_destination_id(@hotel_params['destination_id'])
+    validated_params['location_attributes'] = validate_and_modify_location_attributes(@hotel_params['location_attributes'])
+    validated_params['amenities_attributes'] = validate_and_modify_amenities_attributes(@hotel_params['amenities_attributes'])
+    validated_params['images_attributes'] = validate_and_modify_images_attributes(@hotel_params['images_attributes'])
+    validated_params['booking_conditions_attributes'] = validate_and_modify_booking_conditions_attributes(@hotel_params['booking_conditions_attributes'])
     validated_params.reject! { |_k, v| v.nil? }
     validated_params
   end
 
-  def validate_name(name)
+  def validate_and_modify_name(name)
     if name.length > (@hotel&.name&.length || 0)
       name
     else
@@ -31,7 +34,7 @@ class HotelsPersistentService
     end
   end
 
-  def validate_description(description)
+  def validate_and_modify_description(description)
     if (description&.length || 0) > (@hotel&.description&.length || 0)
       description
     else
@@ -39,11 +42,11 @@ class HotelsPersistentService
     end
   end
 
-  def validate_destination_id(destination_id)
+  def validate_and_modify_destination_id(destination_id)
     destination_id
   end
 
-  def validate_location_attributes(location_attributes)
+  def validate_and_modify_location_attributes(location_attributes)
     model_location = @hotel.location
     updated_attributes = {}
     updated_attributes['lat'] = [model_location&.lat || 0, location_attributes['lat'] || 0].max if location_attributes['lat'].present?
@@ -68,15 +71,36 @@ class HotelsPersistentService
     updated_attributes
   end
 
-  def validate_amenities_attributes(amenities_attributes)
-    amenities_attributes
+  def validate_and_modify_amenities_attributes(amenities_attributes)
+    cleaned_amenities_attributes = []
+    amenities = amenities_attributes.map{|amenity| amenity.merge(hotel_id: @hotel_params['hotel_id'])} rescue []
+    amenities.each do |amenity|
+      if Amenity.where(amenity).empty?
+        cleaned_amenities_attributes << amenity.reject{|k,v| k=="hotel_id"}
+      end
+    end
+    cleaned_amenities_attributes
   end
 
-  def validate_images_attributes(images_attributes)
-    images_attributes
+  def validate_and_modify_images_attributes(images_attributes)
+    cleaned_images_attributes = []
+    images = images_attributes.map{|image| image.merge(hotel_id: @hotel_params['hotel_id'])} rescue []
+    images.each do |image|
+      if Image.where(image).empty?
+        cleaned_images_attributes << image.reject{|k,v| k=="hotel_id"}
+      end
+    end
+    cleaned_images_attributes
   end
 
-  def validate_booking_conditions_attributes(booking_conditions_attributes)
-    booking_conditions_attributes
+  def validate_and_modify_booking_conditions_attributes(booking_conditions_attributes)
+    booking_conditions_attributes ||= []
+    cleaned_booking_conditions_attributes = []
+    booking_conditions_attributes.each do |booking_condition|
+      if BookingCondition.where(booking_condition.merge(hotel_id: @hotel_params['hotel_id'])).empty?
+        cleaned_booking_conditions_attributes << booking_condition
+      end
+    end
+    cleaned_booking_conditions_attributes
   end
 end
