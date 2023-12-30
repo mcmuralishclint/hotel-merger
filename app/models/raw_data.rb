d61df12689f4bef758d9d02802f3570727ce7ea3
Muralish <mcmuralishclint@outlook.com>
2023-12-30 16:25:47 +0530

    use raw data collection to store the raw ingested data

---
+++ app/models/raw_data.rb
@@ -0,0 +1,19 @@
+# app/models/hotels.rb
+class RawData
+  include Mongoid::Document
+  field :hotel_id, type: String
+  field :source, type: String
+  field :name, type: String
+  field :description, type: String
+  field :destination_id, type: String
+  field :location_attributes, type: String
+  field :amenities_attributes, type: String
+  field :images_attributes, type: String
+  field :booking_conditions_attributes, type: String
+
+  def self.upsert_from_params(validated_params)
+    hotel_raw_record = RawData.find_or_initialize_by(hotel_id: validated_params[:hotel_id], source: validated_params[:source])
+    hotel_raw_record.attributes = validated_params
+    hotel_raw_record.save!
+  end
+end
