db0af1929c0179624ecdf9685ac5dd9edc2ba96c
Muralish <mcmuralishclint@outlook.com>
2024-01-01 16:46:20 +0530

    refactor: remove unwanted comments from ingestion spec

--- spec/requests/api/v1/ingestion_spec.rb
+++ spec/requests/api/v1/ingestion_spec.rb
@@ -19,21 +19,14 @@ RSpec.describe 'api/v1/ingestion', type: :request do
         description: 'Use ["*"] to ingest from all the suppliers, ["acme"] from acme only, ["acme","patagonia"] from acme and patagonia only'
       }
 
-      # Example request for ingesting all suppliers
       let(:all_suppliers) { { suppliers: ["*"] } }
       let(:specific_suppliers) { { suppliers: ["acme", "patagonia", "paperflies"] } }
 
-      # Example response for successful ingestion
       response '200', 'Data ingestion started' do
         schema type: :object,
                properties: {
                  message: { type: :string }
                }
-        run_test!
-      end
-
-      # Example response for unsuccessful request
-      response '422', 'Unprocessable Entity' do
         run_test!
       end
     end
